e5bccf9696ecda908ac1445d83edd21ec419d986
ace34TT <tafinasoa35@gmail.com>
2024-06-02 14:50:23 +0300

    fix : git ignore

--- scripts/3_data_analysis/packages.r
+++
@@ -1,9 +0,0 @@
-options(repos = c(CRAN = "https://cloud.r-project.org"))
-
-required_packages <- c("RJDBC", "rJava", "ggplot2", "ggfortify", "RColorBrewer", "dplyr", "rpart", "rpart.plot", "ROracle")
-
-for (package in required_packages) {
-    if (!requireNamespace(package, quietly = TRUE)) {
-        install.packages(package)
-    }
-}
